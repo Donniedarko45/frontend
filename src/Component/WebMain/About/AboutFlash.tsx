dac0f27e39a31a8412b1189a797eb61c0905b65f
Kartikey Pandey <134577536+Donniedarko45@users.noreply.github.com>
2025-02-15 12:05:41 +0530

    Update AboutFlash.tsx

--- src/Component/WebMain/About/AboutFlash.tsx
+++ src/Component/WebMain/About/AboutFlash.tsx
@@ -37,17 +37,20 @@ function AboutFlash({ scrollPosition }: { scrollPosition: any }){
                </Row>
           {visible && (
   <Row style={{ cursor: "pointer" ,width:'100%'}} className="m-auto" >
-   <video className="rounded-2"
+      <video className="rounded-2"
       style={{maxHeight:'610px',height:'auto',width:'100%'}}
       preload="auto"
       autoPlay
       loop
+      muted
       playsInline
+      controls
     >
       <source 
         src="https://res.cloudinary.com/die8tcfj1/video/upload/v1739468129/WhatsApp_Video_2025-02-13_at_23.04.08_729638de_qlvkne.mp4"
         type="application/x-mpegURL"
       />
+      Your browser does not support the video tag.
     </video>
   </Row>
 )}
